720a3b140a76bb38324df4ce319ca5d1264bd8b8
Ryan Weaver <12052413+rdweaver@users.noreply.github.com>
2022-06-27 16:58:20 -0400

    fix copy paste erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,54 @@
 
-import { Providers, ProviderState } from '@microsoft/mgt-element';​
-import { Agenda, Login } from '@microsoft/mgt-react';​
-import React, { useState, useEffect } from 'react';​
+import { Providers, ProviderState } from '@microsoft/mgt-element';
+import { Agenda, Login } from '@microsoft/mgt-react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';​
+import './App.css';
 
-​
-function useIsSignedIn(): [boolean] {​
-  const [isSignedIn, setIsSignedIn] = useState(false);​
-​
-  useEffect(() => {​
-    const updateState = () => {​
-      const provider = Providers.globalProvider;​
-      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);​
-    };​
-​
-    Providers.onProviderUpdated(updateState);​
-    updateState();​
-​
-    return () => {​
-      Providers.removeProviderUpdatedListener(updateState);​
-    }​
-  }, []);​
-​
-  return [isSignedIn];​
-}​
-​
-function App() {​
-  const [isSignedIn] = useIsSignedIn();​
-​
-  return (​
-    <div className="App">​
 
-      <header>​
+function useIsSignedIn(): [boolean] {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    }
+  }, []);
+
+  return [isSignedIn];
+}
+
+function App() {
+  const [isSignedIn] = useIsSignedIn();
+
+  return (
+    <div className="App">
+
+      <header>
       <img src={logo} className="App-logo" alt="logo" />
-        <Login />​
+        <Login />
 
-      </header>​
-      <div>​
+      </header>
+      <div>
 
-        {isSignedIn &&​
+        {isSignedIn &&
 
-          <Agenda />}​
+          <Agenda />}
 
       </div>
-    </div>​
+    </div>
 
-  );​
+  );
 
-}​
+}
 
-​
+
 export default App;
