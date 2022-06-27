425e91ecb5e9a53e54e6944e701850c02b24cb8f
Ryan Weaver <12052413+rdweaver@users.noreply.github.com>
2022-06-27 16:49:52 -0400

    add MGT auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import React from 'react';
+
+import { Providers, ProviderState } from '@microsoft/mgt-element';​
+import { Agenda, Login } from '@microsoft/mgt-react';​
+import React, { useState, useEffect } from 'react';​
 import logo from './logo.svg';
-import './App.css';
+import './App.css';​
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Polite flower? who comes up with these names?
-        </p>
-        <p>
-          My very own SPA
-        </p>
-    
-      </header>
-    </div>
-  );
-}
+​
+function useIsSignedIn(): [boolean] {​
+  const [isSignedIn, setIsSignedIn] = useState(false);​
+​
+  useEffect(() => {​
+    const updateState = () => {​
+      const provider = Providers.globalProvider;​
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);​
+    };​
+​
+    Providers.onProviderUpdated(updateState);​
+    updateState();​
+​
+    return () => {​
+      Providers.removeProviderUpdatedListener(updateState);​
+    }​
+  }, []);​
+​
+  return [isSignedIn];​
+}​
+​
+function App() {​
+  const [isSignedIn] = useIsSignedIn();​
+​
+  return (​
+    <div className="App">​
 
+      <header>​
+      <img src={logo} className="App-logo" alt="logo" />
+        <Login />​
+
+      </header>​
+      <div>​
+
+        {isSignedIn &&​
+
+          <Agenda />}​
+
+      </div>
+    </div>​
+
+  );​
+
+}​
+
+​
 export default App;
